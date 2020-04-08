a56c545a08ed5e66c1714f10ba970ccb3ed54556
YogaVicky <yogasrivarshan@gmail.com>
2020-04-08 17:41:18 +0530

    update

---
+++ simple.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 20;
+console.log(a+b);
+const s = "This is a string";
+console.log(s);
+let fname = "King",lname = "Yoga";
+const name = fname + " " + lname;
+console.log(name);
